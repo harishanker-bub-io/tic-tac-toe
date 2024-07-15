4ae43b8827d46aa70e10154eee99e3843240ef17
harishanker-bub-io <harishanker.500apps@gmail.com>
2024-07-15 12:09:03 +0530

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,22 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devServer:{
+    port: 8000
+  },
+
+  compatibilityDate: '2024-04-03',
+  devtools: { enabled: true },
+
+  app : {
+   head: {
+     script: [
+       {
+         src: 'https://unpkg.com/peerjs@1.5.2/dist/peerjs.min.js',
+         async: true,
+       },
+     ],
+   },
+  },
+
+  modules: ['@nuxtjs/tailwindcss']
+})
